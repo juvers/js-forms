56b5cfb936e33f99f774b49480ffc84624e36293
Jude K <judekuti@gmail.com>
2021-04-28 22:21:40 -0500

    Add scaffolding

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
-
+  const data = watch("example");
   console.log(watch("example")); // watch input value by passing the name of it
 
   return (
@@ -24,6 +24,8 @@ export default function App() {
       {errors.exampleRequired && <span>This field is required</span>}
 
       <input type="submit" />
+
+      <p>{JSON.stringify(data)}</p>
     </form>
   );
 }
